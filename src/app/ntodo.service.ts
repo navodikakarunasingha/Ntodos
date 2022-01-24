74c940354571c1c0479ae4dd434c2837fd60080f
navodikakarunasingha <navodikaefac@gmail.com>
2022-01-25 01:14:00 +1100

    remove assets import

--- src/app/ntodo.service.ts
+++ src/app/ntodo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { INtodo } from '../assets/data/ntodo';
+
 
 @Injectable()
 export class NtodoService {
